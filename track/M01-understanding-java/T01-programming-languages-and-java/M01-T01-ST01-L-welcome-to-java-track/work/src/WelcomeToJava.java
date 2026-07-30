c32f49b987e5c7af1ce762bf32f7b4b9e03d0f11
Nandini Reddy <nandinimallikireddy15@gmail.com>
2026-07-30 13:08:46 +0530

    add one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-      System.out.println("Welcome to the java track");
-      System.out.println("I already know how to build programming logic");
-      System.out.println("Now i am learning how to express that logic using java");
+        System.out.println("Welcome to the java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now i am learning how to express that logic using java");
+        System.out.println("Now i am learning how to use git  and github");
     }
 }

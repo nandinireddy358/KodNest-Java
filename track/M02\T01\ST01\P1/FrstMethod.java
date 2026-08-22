95b6b5a916e9fc8c91a868210f9bf0c14eb98f26
Nandini Reddy <nandinimallikireddy15@gmail.com>
2026-08-22 17:09:50 +0530

    FrstMethod

---
+++ track/M02\T01\ST01\P1/FrstMethod.java
@@ -0,0 +1,18 @@
+
+class Robot {
+
+    void speak() {
+        // Print the message
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+public class FrstMethod {
+
+    public static void main(String[] args) {
+        // Create a Robot object
+        Robot obj = new Robot();
+        // Call speak()
+        obj.speak();
+    }
+}

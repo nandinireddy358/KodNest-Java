8247a8c3d30113b11e5a773620ac2135652809f6
Nandini Reddy <nandinimallikireddy15@gmail.com>
2026-08-25 16:07:06 +0530

    Frequency

---
+++ track/M02\T01\ST01\P1/Frequency.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Frequency {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Frequency of " + target + ": " + count);
+    }
+}

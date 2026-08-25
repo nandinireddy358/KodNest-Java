173a1b3fc0c7e3aa4f7de109f14a77f494a6fd0c
Nandini Reddy <nandinimallikireddy15@gmail.com>
2026-08-25 15:36:55 +0530

    FrstConstructor

---
+++ track/M02\T01\ST01\P1/FrstConstructor.java
@@ -0,0 +1,16 @@
+
+class Robot {
+
+    Robot() {
+        // Print the message
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class FrstConstructor {
+
+    public static void main(String[] args) {
+        // Create one Robot object
+        Robot obj = new Robot();
+    }
+}
